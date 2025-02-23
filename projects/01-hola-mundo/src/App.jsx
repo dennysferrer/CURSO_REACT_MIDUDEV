a0cf0a1b8a0d5564d9edf3edb7d3fa879ac1a04b
dennysferrer <dennys.ferrer@gmail.com>
2025-02-23 16:40:35 -0500

    Se culminó sesión con la implementación del estado isFollowing

--- projects/01-hola-mundo/src/App.jsx
+++ projects/01-hola-mundo/src/App.jsx
@@ -3,11 +3,18 @@ import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
 export const App = () => {
+    const formatUserName = (username) => {
+        return `@${username}`;
+    }
     return (
-        <>
-            <TwitterFollowCard userName='midudev' name='Miguel Angel Duran' />
-            <TwitterFollowCard userName='pheralb' name='Pablo Hernandez' />
-        </>
+        <seccion className='App'>
+            <TwitterFollowCard formatUserName={formatUserName} userName='midudev'>
+                Miguel Angel Duran
+            </TwitterFollowCard>
+            <TwitterFollowCard formatUserName={formatUserName} userName='pheralb'>
+                Pablo Hernandez
+            </TwitterFollowCard>
+        </seccion>
         
     )
 }
--- projects/01-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/01-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 
-export const TwitterFollowCard = ({userName, name, isFollowing}) => {
+export const TwitterFollowCard = ({formatUserName, userName, children}) => {
+    const [ isFollowing, setIsFollowing ] = useState(false)
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    const text = isFollowing ? 'Siguiendo':'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
     const imgSrc = `https://unavatar.io/${userName}`
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
                 <img className="tw-followCard-avatar" src={imgSrc} alt="avatar" />
                 <div className="tw-followCard-info">
-                    <strong>{name}</strong>
-                    <span className="tw-followCard-infoUsername">@{userName}</span>
+                    <strong>{children}</strong>
+                    <span className="tw-followCard-infoUsername">{formatUserName(userName)}</span>
                 </div>
             </header>
             <aside>
-                <button className="tw-followCard-button">Seguir</button>
+                <button className={buttonClassName} onClick={handleClick}>{text}</button>
             </aside>
         </article>
     )
